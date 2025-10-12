ea21a889579d2fe7896f7dcf46fe29811bc44c1f
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2025-10-12 17:58:45 +0800

    Add UI components

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+from tkinter import filedialog
 
 from reader import ingest_dates
 from writer import WriterConfig, writer
@@ -36,14 +37,42 @@ writer_config = WriterConfig(output_path=output_path, output_file_name=output_fi
                              duty_month=duty_month, public_holidays=public_holidays,
                              show_individual_blocked_pct=show_individual_blocked_pct)
 
-# root = Tk()
-#
-# root.title("Duty Planner")
-# root.geometry("500x400")
-#
-# label = Label(root, text="testing")
-# label.pack()
-#
-# root.mainloop()
+window = Tk()
 
-writer(duty_personnel, writer_config)
+window.title("Duty planner")
+window.geometry("500x400")
+
+
+def select_folder(label: Label, kind: str):
+    def f():
+        folder = filedialog.askdirectory(title=f"Select {kind} folder")
+
+        if folder != "":
+            label.configure(text=f"Selected {kind} folder: {folder}")
+
+    return f
+
+
+def run_writer():
+    writer(duty_personnel, writer_config)
+
+
+input_folder_label = Label(window, text="No input folder selected")
+input_folder_label.pack()
+open_input_folder = Button(window, text="Select input folder", command=select_folder(input_folder_label, "input"))
+open_input_folder.pack()
+
+output_folder_label = Label(window, text="No output folder selected")
+output_folder_label.pack()
+open_output_folder = Button(window, text="Select output folder", command=select_folder(output_folder_label, "output"))
+open_output_folder.pack()
+
+output_file_name_label = Label(window, text="File name (ending in .xlsx)")
+output_file_name_label.pack()
+output_file_name = Entry(window)
+output_file_name.pack()
+
+generate_file = Button(window, text="Generate", command=run_writer)
+generate_file.pack()
+
+window.mainloop()
