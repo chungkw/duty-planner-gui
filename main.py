c424653b865653667f9955ced16bae52dc42fac0
Chung Kin Wai <73325564+chungkw@users.noreply.github.com>
2025-10-13 15:41:50 +0800

    Add functional UI elements
    for input/output dir, show individual blocked pct and file name

--- main.py
+++ main.py
@@ -5,74 +5,91 @@ from tkinter import filedialog
 from reader import ingest_dates
 from writer import WriterConfig, writer
 
-input_path = "D:\\Coding\\duty-planner-gui\\data\\apr25"
-output_path = "D:\\Coding\\duty-planner-gui\\output"
-output_file_name = "apr25 test b.xlsx"
-
 duty_year = 2025
 duty_month = 4
 public_holidays = [18]
 
-show_individual_blocked_pct = True
-
-dir_scan = os.scandir(input_path)
-
-# Take every file's path inside the folder
-dir_files = [entry.path for entry in dir_scan if entry.is_file()]
-
-# Sort into alphabetical order
-# This method will mutate the list itself
-dir_files.sort()
-
-duty_personnel = []
-
-for file_path in dir_files:
-    file = open(file_path, "r")
-    text = file.read()
-    ingested = ingest_dates(text)
-    duty_personnel.append(ingested)
-    file.close()
-
-writer_config = WriterConfig(output_path=output_path, output_file_name=output_file_name, duty_year=duty_year,
-                             duty_month=duty_month, public_holidays=public_holidays,
-                             show_individual_blocked_pct=show_individual_blocked_pct)
-
 window = Tk()
+window.title("Duty Planner")
 
-window.title("Duty planner")
-window.geometry("500x400")
+input_path = StringVar()
+output_path = StringVar()
+show_individual_blocked_pct = IntVar()
 
 
-def select_folder(label: Label, kind: str):
+def select_input_folder(label: Label):
     def f():
-        folder = filedialog.askdirectory(title=f"Select {kind} folder")
+        folder_path = filedialog.askdirectory(title=f"Select input folder")
 
-        if folder != "":
-            label.configure(text=f"Selected {kind} folder: {folder}")
+        if folder_path != "":
+            label.configure(text=f"Selected input folder: {folder_path}")
+            input_path.set(folder_path)
+
+    return f
+
+
+def select_output_folder(label: Label):
+    def f():
+        folder_path = filedialog.askdirectory(title=f"Select output folder")
+
+        if folder_path:
+            label.configure(text=f"Selected output folder: {folder_path}")
+            output_path.set(folder_path)
 
     return f
 
 
 def run_writer():
+    dir_scan = os.scandir(input_path.get())
+
+    # Take every file's path inside the folder
+    dir_files = [entry.path for entry in dir_scan if entry.is_file()]
+
+    # Sort into alphabetical order
+    # This method will mutate the list itself
+    dir_files.sort()
+
+    duty_personnel = []
+
+    for file_path in dir_files:
+        file = open(file_path, "r")
+        text = file.read()
+        ingested = ingest_dates(text)
+        duty_personnel.append(ingested)
+        file.close()
+
+    file_name = output_file_name.get()
+    file_name = file_name if file_name.endswith(".xlsx") else file_name + ".xlsx"
+
+    writer_config = WriterConfig(output_path=output_path.get(), output_file_name=file_name,
+                                 duty_year=duty_year,
+                                 duty_month=duty_month, public_holidays=public_holidays,
+                                 show_individual_blocked_pct=bool(show_individual_blocked_pct.get()))
+
     writer(duty_personnel, writer_config)
 
 
 input_folder_label = Label(window, text="No input folder selected")
-input_folder_label.pack()
-open_input_folder = Button(window, text="Select input folder", command=select_folder(input_folder_label, "input"))
-open_input_folder.pack()
+input_folder_label.grid(row=1, column=1, sticky="nsew")
+open_input_folder = Button(window, text="Select input folder", command=select_input_folder(input_folder_label))
+open_input_folder.grid(row=1, column=2, sticky="nsew")
 
 output_folder_label = Label(window, text="No output folder selected")
-output_folder_label.pack()
-open_output_folder = Button(window, text="Select output folder", command=select_folder(output_folder_label, "output"))
-open_output_folder.pack()
+output_folder_label.grid(row=2, column=1, sticky="nsew")
+open_output_folder = Button(window, text="Select output folder", command=select_output_folder(output_folder_label))
+open_output_folder.grid(row=2, column=2, sticky="nsew")
 
-output_file_name_label = Label(window, text="File name (ending in .xlsx)")
-output_file_name_label.pack()
+output_file_name_label = Label(window, text="File name")
+output_file_name_label.grid(row=3, column=1, sticky="nsew")
 output_file_name = Entry(window)
-output_file_name.pack()
+output_file_name.grid(row=3, column=2, sticky="nsew")
+
+show_each_blocked_label = Label(window, text="Show individual blocked?")
+show_each_blocked_label.grid(row=4, column=1, sticky="nsew")
+show_each_blocked = Checkbutton(window, onvalue=1, offvalue=0, variable=show_individual_blocked_pct)
+show_each_blocked.grid(row=4, column=2, sticky="nsew")
 
 generate_file = Button(window, text="Generate", command=run_writer)
-generate_file.pack()
+generate_file.grid(row=5, column=1, sticky="nsew")
 
 window.mainloop()
